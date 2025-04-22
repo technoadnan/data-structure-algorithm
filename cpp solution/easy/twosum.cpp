198363023fe15fe262198169d59e9b09d8e103ba
Adnan Bin Alam <code.technoadnan@gmail.com>
2025-04-21 23:08:12 -0500

    Add TwoSum implementation

---
+++ cpp solution/easy/twosum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < nums.size(); ++i) {
+            int need = target - nums[i];
+            if (mpp.contains(need)) {
+                return {mpp[need], i};
+            }
+            mpp[nums[i]] = i;
+        }
+        return {}; // Fallback, though problem guarantees a solution
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+
+    for (int index : result) {
+        cout << index << " ";
+    }
+    cout << endl;
+    return 0;
+}
